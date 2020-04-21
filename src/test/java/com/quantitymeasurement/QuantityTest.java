65f561b24079d33b126bb0828a00b9d81db2c0ca
ARATI-UPARE <aratiupare@gmail.com>
2020-04-21 22:54:07 +0530

    ADDED:Test to Check Reference is same shoulde Return Equal

--- src/test/java/com/quantitymeasurement/QuantityTest.java
+++ src/test/java/com/quantitymeasurement/QuantityTest.java
@@ -20,8 +20,14 @@ public class QuantityTest {
     }
     // TC:1.2
     @Test
-    public void givenOf1NullObject_ShouldreturnFalse() {
+    public void givenOf1NullObject_ShouldreturnNotEqual() {
         Feet feet=new Feet(0.0);
         Assert.assertNotEquals(feet,null);
+    }
+    // TC:1.3
+    @Test
+    public void givenOfReferencefeetAndFeet_ShouldreturnEqual() {
+        Feet feet=new Feet(0.0);
+        Assert.assertEquals(feet,feet);
     }
 }
