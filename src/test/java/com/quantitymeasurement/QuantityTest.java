07e07082ab314c398e985b4de7beb40a1c239be8
ARATI-UPARE <aratiupare@gmail.com>
2020-04-21 23:09:24 +0530

    ADDED:Test to Check Type  is Same shoulde Return Equal

--- src/test/java/com/quantitymeasurement/QuantityTest.java
+++ src/test/java/com/quantitymeasurement/QuantityTest.java
@@ -12,12 +12,6 @@ public class QuantityTest {
         Feet feet2=new Feet(0.0);
         Assert.assertEquals(feet1,feet2);
     }
-    @Test
-    public void givenOf0FeetAnd1Feet_ShouldreturnNotEqual() {
-        Feet feet1=new Feet(0.0);
-        Feet feet2=new Feet(1.0);
-        Assert.assertNotEquals(feet1,feet2);
-    }
     // TC:1.2
     @Test
     public void givenOf1NullObject_ShouldreturnNotEqual() {
@@ -30,9 +24,18 @@ public class QuantityTest {
         Feet feet=new Feet(0.0);
         Assert.assertEquals(feet,feet);
     }
+    //TC:1.4
     @Test
     public void givenStringAndFeet_whenDifferentType_shouldReturnNotEqual() {
         Feet feet = new Feet(0.0);
         Assert.assertNotEquals(feet, "Type");
     }
+    //TC:1.5
+    @Test
+    public void givenFeetAndFeet_whenSameType_shouldReturnEqual() {
+        Feet feet1 = new Feet(0.0);
+        Feet feet2 = new Feet(0.0);
+        Assert.assertEquals(feet1, feet2);
+    }
+
 }
