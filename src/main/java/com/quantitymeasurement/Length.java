5764ca196f1dd1f920df0616925d52cf65cc062e
ARATI-UPARE <aratiupare@gmail.com>
2020-04-22 00:02:35 +0530

    Refactored: Code as DRY was Violated for Feet And Inch with Length And Unit to Differentiate

---
+++ src/main/java/com/quantitymeasurement/Length.java
@@ -0,0 +1,25 @@
+package com.quantitymeasurement;
+
+import java.util.Objects;
+
+public class Length {
+
+    enum Unit {FEET,INCH};
+    private final Unit unit;
+    private final double value;
+
+    public Length(Unit unit,double value) {
+        this.unit = unit;
+        this.value = value;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Length length = (Length) obj;
+        return Double.compare(length.value, value) == 0 &&
+                unit == length.unit;
+    }
+
+}
--- src/test/java/com/quantitymeasurement/QuantityTest.java
+++ src/test/java/com/quantitymeasurement/QuantityTest.java
@@ -8,67 +8,67 @@ public class QuantityTest {
     // TC:1.1
     @Test
     public void givenOf0FeetAnd0Feet_ShouldReturnEqual() {
-        Feet feet1=new Feet(0.0);
-        Feet feet2=new Feet(0.0);
+        Length feet1=new Length(Length.Unit.FEET,0.0);
+        Length feet2=new Length(Length.Unit.FEET,0.0);
         Assert.assertEquals(feet1,feet2);
     }
     // TC:1.2
     @Test
     public void givenOf1NullFeetObject_ShouldReturnNotEqual() {
-        Feet feet=new Feet(0.0);
+        Length feet=new Length(Length.Unit.FEET,0.0);
         Assert.assertNotEquals(feet,null);
     }
     // TC:1.3
     @Test
     public void givenOfReferencefeetAndFeet_ShouldReturnEqual() {
-        Feet feet=new Feet(0.0);
+        Length feet=new Length(Length.Unit.FEET,0.0);
         Assert.assertEquals(feet,feet);
     }
     //TC:1.4
     @Test
     public void givenStringAndFeet_whenDifferentType_shouldReturnNotEqual() {
-        Feet feet = new Feet(0.0);
+        Length feet = new Length(Length.Unit.FEET,0.0);
         Assert.assertNotEquals(feet, "Type");
     }
     //TC:1.5
     @Test
     public void givenOf0FeetAnd1Feet_whenDifferentValues_ShouldreturnNotEqual() {
-        Feet feet1 = new Feet(0.0);
-        Feet feet2 = new Feet(1.0);
+        Length feet1 = new Length(Length.Unit.FEET,0.0);
+        Length feet2 = new Length(Length.Unit.FEET,1.0);
         Assert.assertNotEquals(feet1,feet2);
     }
 
     // TC:1.6
     @Test
     public void givenOf0InchAnd0Inch_ShouldReturnEqual() {
-        Inch inch1 = new Inch(0.0);
-        Inch inch2 = new Inch(0.0);
+        Length inch1 = new Length(Length.Unit.INCH,0.0);
+        Length inch2 = new Length(Length.Unit.INCH,0.0);
         Assert.assertEquals(inch1,inch2);
     }
     // TC:1.7
     @Test
     public void givenOf1NullInchObject_ShouldReturnNotEqual() {
-        Inch inch = new Inch(0.0);
+        Length inch = new Length(Length.Unit.INCH,0.0);
         Assert.assertNotEquals(inch,null);
     }
     // TC:1.8
     @Test
     public void givenOfReferenceInchAndInch_ShouldReturnEqual() {
-        Inch inch = new Inch(0.0);
+        Length inch = new Length(Length.Unit.INCH,0.0);
         Assert.assertEquals(inch,inch);
     }
     //TC:1.9
     @Test
     public void givenInchAndFeet_whenDifferentType_shouldReturnNotEqual() {
-        Feet feet = new Feet(0.0);
-        Inch inch = new Inch(0.0);
+        Length feet = new Length(Length.Unit.FEET,0.0);
+        Length inch = new Length(Length.Unit.INCH,0.0);
         Assert.assertNotEquals(feet, inch);
     }
     //TC:1.10
     @Test
     public void givenOf0InchAnd1Inch_whenDifferentValues_ShouldreturnNotEqual() {
-        Inch inch1 = new Inch(0.0);
-        Inch inch2 = new Inch(1.0);
+        Length inch1 = new Length(Length.Unit.INCH,0.0);
+        Length inch2 = new Length(Length.Unit.INCH,1.0);
         Assert.assertNotEquals(inch1,inch2);
     }
 }
